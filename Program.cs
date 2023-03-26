ccb8dbb508f22504aa7c56735571c55b2ba319be
Joe Sutter <jsutter6@my.wctc.edu>
2023-03-26 18:30:30 -0500

    {1.01} add code

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
 ﻿using System.Linq;
+using NLog;
+string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
 string choice;
+
+
+
+
 List<Ticket> ticketList = new List<Ticket>();
 
 do
@@ -12,8 +19,12 @@ do
     choice = Console.ReadLine();
 
     if (choice == "1")
-
+        
     {
+        Console.WriteLine("Enter the number for the type of ticket:\n 1.Bug/Defect\n 2.Enhancement\n 3.Task ");
+        string type = Console.ReadLine();
+        
+        
         Ticket ticket = new Ticket();
 
         ticket.ticketId = ticketList.Count == 0 ? 1 : ticketList.Max(t => t.ticketId) + 1;
@@ -24,7 +35,7 @@ do
         Console.WriteLine("Enter Ticket Status");
         ticket.status = Console.ReadLine().ToUpper();
 
-        Console.WriteLine("Enter Ticket Priority");
+        Console.WriteLine("Enter Ticket Priority(High,Medium,Low)");
         ticket.priority = Console.ReadLine().ToUpper();
 
         Console.WriteLine("Who Submitted Ticket?");
@@ -36,9 +47,54 @@ do
         Console.WriteLine("Who is Watching Ticket?");
         ticket.watch = Console.ReadLine().ToUpper();
 
-        ticketList.Add(ticket);
+        if (type == "1")
+        {
+        Console.WriteLine("What is the Severity of the ticket?(High,Medium,Low)");
+        ticket.severity = Console.ReadLine().ToUpper();
+        }
 
+        else if (type == "2")
+    {
+        Console.WriteLine("What is the software you are editing?");
+       string edit = Console.ReadLine();
+
+       Console.WriteLine("What is the cost?");
+       string dollars = Console.ReadLine();
+
+       Console.WriteLine("What is the reason for the enhancement?");
+       string why = Console.ReadLine();
+
+       Console.WriteLine("What is the estimated cost?");
+       string estimatedCost = Console.ReadLine();
+
+        Enhancement enhancement = new Enhancement
+        {
+           software = edit,
+           cost = dollars,
+           reason = why,
+           estimate = estimatedCost
+        };
+
+    }    
+    
+
+    else if (type == "3")
+    {
+        Console.WriteLine("What is the Project Name?");
+       string name = Console.ReadLine();
+
+       Console.WriteLine("What is the due date of the project?");
+       string date = Console.ReadLine();
+
+       Task task = new Task
+       {
+        project = name,
+        dueDate = date
+       };
     }
+    ticketList.Add(ticket);
+    }
+        
     
     else if (choice == "2")
  {
@@ -48,3 +104,5 @@ do
         }
     }
 } while (choice == "1" || choice == "2");
+
+//logger.Info("Program ended");
--- Ticket.cs
+++ Ticket.cs
@@ -7,10 +7,36 @@ public class Ticket
     public string owner { get; set; }
     public string assign { get; set; }
     public string watch { get; set; }
+    public string severity { get; set;}
 
-    public string Display()
+    public virtual string Display()
     {
-        return $"Id: {ticketId}\nSummary: {summary}\nStatus: {status}\nPriority: {priority}\nSubmitted by: {owner}\nAssigned to: {assign}\nWatched by: {watch}\n";
+        return $"Id: {ticketId}\nSummary: {summary}\nStatus: {status}\nPriority: {priority}\nSubmitted by: {owner}\nAssigned to: {assign}\nWatched by: {watch}\nSeverity Level:{severity}";
+    }
+}
+
+public class Enhancement : Ticket
+{
+    public string software { get; set;}
+    public string cost { get; set;}
+    public string reason {get; set;}
+    public string estimate { get; set;}
+
+    public override string Display()
+    {
+        return $"Id: {ticketId}\nSummary: {summary}\nStatus: {status}\nPriority: {priority}\nSubmitted by: {owner}\nAssigned to: {assign}\nWatched by: {watch}\nSoftware Enhanced: {software}\nCost: {cost}\nReason: {reason}\nEstimate: {estimate}\n";
+    }
+
+}
+
+public class Task : Ticket
+{
+    public string project {get; set;}
+    public string dueDate {get; set;}
+
+    public override string Display()
+    {
+        return $"Id: {ticketId}\nSummary: {summary}\nStatus: {status}\nPriority: {priority}\nSubmitted by: {owner}\nAssigned to: {assign}\nWatched by: {watch}\nProject Name: {project}\nDue Date: {dueDate}\n";
     }
 }
 
